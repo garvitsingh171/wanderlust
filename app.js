09c3e0f00715dc3f71f23e8d911e7d9dc5726e68
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-02-24 14:07:31 +0530

    prittier setup

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const mongoose = require("mongoose");
 const app = express();
 
 async function connectDB() {
-  mongoose.connect("mongodb://localhost:27017/wanderlust");
+    mongoose.connect("mongodb://localhost:27017/wanderlust");
 }
 
 connectDB()
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-  });
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+    });
 
 app.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+    console.log("Server is running at http://localhost:3000");
 });
