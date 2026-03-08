f887a7cf84c9541a9a5e6cd4028469c5d70b7fff
garvitsingh171 <garvit.singh.s.125@kalvium.community>
2026-03-08 19:17:23 +0530

    Modified codebase for deployment goal

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const app = require('./src/app');
+require('dotenv').config();
+
 const { connectToDB } = require('./src/config/db');
+
+const PORT = process.env.PORT
 
 async function startServer() {
     try {
         await connectToDB();
-        app.listen(3000, () => {
-            console.log("Server is running at http://localhost:3000");
+        app.listen(PORT, () => {
+            console.log(`Server is running at http://localhost:${PORT}`);
         });
     } catch (error) {
         console.error("Failed to start server", error);
--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI;
 
 async function connectToDB() {
     try {
-        await mongoose.connect("mongodb://localhost:27017/wanderlust");
+        if (!MONGO_URI) {
+            throw new Error("MONGO_URI is not defined in environment variables");
+        }
+        await mongoose.connect(MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
     }
 }
-
-connectToDB();
 
 module.exports = { connectToDB };
